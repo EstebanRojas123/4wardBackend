6565790f5f13b3447f90adfcc2c213c6e374daef
Renata <renata.cuello@alumnos.ucn.cl>
2025-05-28 16:54:37 -0400

    conexion base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MqttModule } from './mqtt/mqtt.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Temperatura } from './temperatura/temperatura.entity';
+import { TemperaturaService } from './temperatura/temperatura.service';
+import { TemperaturaController } from './temperatura/temperatura.controller'; 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || '123',
+      database: process.env.DB_NAME || 'estufa',
+      entities: [Temperatura],
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([Temperatura]),
     MqttModule,
   ],
+  providers: [TemperaturaService],
+  controllers: [TemperaturaController],
 })
 export class AppModule {}
